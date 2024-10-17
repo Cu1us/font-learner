56829d182b06557f538211a51aeeaafbc8c69fc7
Cu1us <98174691+Cu1us@users.noreply.github.com>
2024-10-17 21:07:59 +0200

    Misc edits

--- Learner/Assets/Scripts/RunnerManager.cs
+++ Learner/Assets/Scripts/RunnerManager.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RunnerManager : MonoBehaviour
 {
     [SerializeField]
     RunnerPlayer player;
+    [SerializeField]
+    RunnerWall wallPrefab;
+    [SerializeField]
+    Transform wallContainer;
+
+    [SerializeField]
+    TextMeshProUGUI topText;
+    [SerializeField]
+    TextMeshProUGUI scoreText;
 
     Vector2 screenTouchPoint;
+
+    bool correctAnswerIsLeft;
+
+    int score;
+    float timeToAnswer = 4f;
 
     void Start()
     {
@@ -17,6 +32,10 @@ public class RunnerManager : MonoBehaviour
     void Update()
     {
         UpdatePlayerPosition();
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SpawnWall();
+        }
     }
 
     void UpdatePlayerPosition()
@@ -31,5 +50,49 @@ public class RunnerManager : MonoBehaviour
         }
         float playerX = Camera.main.ScreenToViewportPoint(screenTouchPoint).x;
         player.SetPositionX(playerX);
+    }
+
+    void SpawnWall()
+    {
+        RunnerWall wall = Instantiate(wallPrefab, wallContainer);
+        wall.fallDuration = timeToAnswer;
+        wall.hitCallback += OnWallHit;
+
+        string correctWord = GetRandomWord();
+        string fakeWord = GetRandomWord();
+
+        correctAnswerIsLeft = Random.Range(0, 2) == 0; // 50%
+
+        if (correctAnswerIsLeft)
+        {
+            wall.SetText(correctWord, fakeWord);
+        }
+        else
+        {
+            wall.SetText(fakeWord, correctWord);
+        }
+        topText.text = correctWord;
+    }
+    void OnWallHit(RunnerWall wall)
+    {
+        bool success = player.currentX < 0.5f && correctAnswerIsLeft;
+        if (success)
+        {
+            score++;
+            scoreText.text = score.ToString();
+            timeToAnswer *= 0.9f;
+        }
+        else
+        {
+            if (timeToAnswer < 4f)
+            {
+                timeToAnswer = Mathf.Min(timeToAnswer + 0.25f, 4f);
+            }
+            Destroy(wall.gameObject);
+        }
+    }
+    string GetRandomWord(int minLength = 0, int maxLength = int.MaxValue)
+    {
+        return new string[] { "abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vxy" }[Random.Range(0, 3)];
     }
 }
--- Learner/Assets/Scripts/RunnerWall.cs
+++ Learner/Assets/Scripts/RunnerWall.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
+[RequireComponent(typeof(RectTransform))]
 public class RunnerWall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    TextMeshProUGUI LeftLabel;
+    [SerializeField]
+    TextMeshProUGUI RightLabel;
 
+    RectTransform rectTransform;
+
+    public float fallDuration;
+    public float targetY;
+    float startY;
+
+    public bool hasHit = false;
+
+    float fallProgress = 0;
+
+    public Action<RunnerWall> hitCallback;
+
+    public void SetText(string left, string right)
+    {
+        LeftLabel.text = left;
+        RightLabel.text = right;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-
+        rectTransform = GetComponent<RectTransform>();
+        startY = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 1f)).y;
+        UpdatePosition();
+    }
+    private void Update()
+    {
+        fallProgress += 1 / fallDuration * Time.deltaTime;
+        UpdatePosition();
+        if (!hasHit && fallProgress >= 1)
+        {
+            hasHit = true;
+            hitCallback?.Invoke(this);
+        }
+    }
+    void UpdatePosition()
+    {
+        float newY = Mathf.LerpUnclamped(startY, targetY, fallProgress);
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, newY);
     }
 }
